b5a2cc355fdffd580f3e085607a48a906b9825fa
Jingbo Zhang <uceehbb@ucl.ac.uk>
2024-07-17 17:21:23 +0800

    parameterized query added

--- Online_Forum_Safe_Version/app.py
+++ Online_Forum_Safe_Version/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, redirect, url_for, request, jsonify, flash, send_file
+from flask import Flask, render_template, session, redirect, url_for, request, jsonify, flash, send_file
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy import create_engine
 
 app = Flask(__name__)
@@ -14,38 +15,29 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
-    password = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(200), nullable=False)  # hash password
 
 
 def login_1(username, password):
     user = User.query.filter_by(username=username).first()
     if user:
-        if user.password == password:
-            return "Login successful"
+        print(user.password, password)
+        print(check_password_hash(user.password, password))
+        if check_password_hash(user.password, password):
+            return True
         else:
-            return "Invalid password"
-    else:
-        return "User not found"
-
-
-def print_all_users():
-    try:
-        users = User.query.all()
-        print("Current users in the database:")
-        for user in users:
-            print(f"Username: {user.username}, Password: {user.password}")
-            print('finished')
-    except Exception as e:
-        print(f"Error accessing the database: {e}")
+            return False
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
 
+
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
-    return render_template('upload.html')
+    return render_template('upload.html', username=session['username'])
+
 
 @app.route('/')
 def root():
@@ -62,25 +54,27 @@ def login_check():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        captchaName = request.form.get('captchaName')
-        # if captchaName != 'panda':
-        #     flash('Invalid captcha')
-        #     return redirect(url_for('login'))
-        result = login_1(username, password)
-        if result == "Login successful":
+        login_result = login_1(username, password)
+        if login_result:
+            session['username'] = username
             flash('Login successful')
             return redirect(url_for('upload'))
         else:
             flash('Invalid username or password')
             return redirect(url_for('login'))
+        captchaName = request.form.get('captchaName')
+        # if captchaName != 'panda':
+        #     flash('Invalid captcha')
+        #     return redirect(url_for('login'))
 
 
 def add_user(username, password):
     is_exist = User.query.filter_by(username=username).first()
     if is_exist:
-        return "User already exists"
+        print( "User already exists")
     else:
-        new_user = User(username=username, password=password)
+        new_user = User(username=username, password=generate_password_hash(password))
+        print(f"user add successfully,name={username},password={new_user.password}")
         db.session.add(new_user)
         db.session.commit()
 
@@ -89,5 +83,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         add_user('admin', '123')
-        print("User added successfully")
     app.run(debug=True)
